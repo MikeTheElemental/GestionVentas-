76f9634f334599a8c46b8bacd87ae5dea505ba0d
Joaquín Campero <joalab001@gmail.com>
2026-07-02 12:15:01 -0300

    Add mostrarMenu function to display menu options

--- src/menu.js
+++ src/menu.js
@@ -1,1 +1,10 @@
+function mostrarMenu() {
+    console.log("===== MENÚ =====");
+    console.log("1. Ventas");
+    console.log("2. Compras");
+    console.log("3. Stock");
+    console.log("4. Reportes");
+    console.log("5. Salir");
+}
 
+module.exports = { mostrarMenu };
